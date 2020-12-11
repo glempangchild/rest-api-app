933f6afe39faec647b59469a4c72acb4d4910e6b
Dedy Setiawan <glempangchild@gmail.com>
2020-12-11 15:38:22 +0700

    get all data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,17 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req,res) {
-    response.ok("run well")
+    response.ok("run well", res)
+};
+
+// Menampilkan semua data
+
+exports.getAll= function(req,res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows,res)
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@
 module.exports = function(app) {
     var jesonku = require('./controller');
     app.route('/').get(jesonku.index);
+    app.route('/getall').get(jesonku.getAll);
 }
